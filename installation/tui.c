e983e7d5a8af1d84473c2ecfda8ddbaffa13bc43
s42yt <songoku42@outlook.de>
2026-08-09 11:45:11 +0200

    Rename installer TUI colour macros so ERROR stops shadowing the windows.h macro

--- installation/tui.c
+++ installation/tui.c
@@ -18,10 +18,10 @@
 #endif
 
 #define WIDTH 72
-#define ACCENT "\x1b[1;32m"
-#define DIM "\x1b[90m"
-#define ERROR "\x1b[31m"
-#define RESET "\x1b[0m"
+#define TUI_ACCENT "\x1b[1;32m"
+#define TUI_DIM "\x1b[90m"
+#define TUI_ERROR "\x1b[31m"
+#define TUI_RESET "\x1b[0m"
 
 void tui_init(void) {
     HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -33,14 +33,14 @@ void tui_init(void) {
 }
 
 static void rule(void) {
-    fputs(DIM, stdout);
+    fputs(TUI_DIM, stdout);
     for (int i = 0; i < WIDTH; i++) fputs("\xe2\x94\x80", stdout);
-    fputs(RESET "\n", stdout);
+    fputs(TUI_RESET "\n", stdout);
 }
 
 void tui_screen(const char *title) {
     fputs("\x1b[2J\x1b[H\n", stdout);
-    printf("  " ACCENT "FreSH" RESET " " DIM "%s" RESET "\n", FRESH_VERSION);
+    printf("  " TUI_ACCENT "FreSH" TUI_RESET " " TUI_DIM "%s" TUI_RESET "\n", FRESH_VERSION);
     printf("  %s\n", title);
     fputs("  ", stdout);
     rule();
@@ -56,18 +56,18 @@ void tui_blank(void) {
 }
 
 void tui_step(int ok, const char *message) {
-    if (ok) printf("  " ACCENT "\xe2\x9c\x93" RESET " %s\n", message);
-    else printf("  " ERROR "\xe2\x9c\x97" RESET " %s\n", message);
+    if (ok) printf("  " TUI_ACCENT "\xe2\x9c\x93" TUI_RESET " %s\n", message);
+    else printf("  " TUI_ERROR "\xe2\x9c\x97" TUI_RESET " %s\n", message);
     fflush(stdout);
 }
 
 void tui_progress(int done, int total) {
     int filled = total > 0 ? done * 40 / total : 0;
-    printf("\r  " ACCENT);
+    printf("\r  " TUI_ACCENT);
     for (int i = 0; i < filled; i++) fputs("\xe2\x96\x88", stdout);
-    fputs(DIM, stdout);
+    fputs(TUI_DIM, stdout);
     for (int i = filled; i < 40; i++) fputs("\xe2\x96\x91", stdout);
-    printf(RESET " %d%%", total > 0 ? done * 100 / total : 100);
+    printf(TUI_RESET " %d%%", total > 0 ? done * 100 / total : 100);
     fflush(stdout);
 }
 
@@ -79,8 +79,8 @@ int tui_menu(const char **options, int count) {
         if (drawn) printf("\x1b[%dA", count);
         for (int i = 0; i < count; i++) {
             printf("\r\x1b[K");
-            if (i == selected) printf("  " ACCENT "\xe2\x9d\xaf %s" RESET "\n", options[i]);
-            else printf("    " DIM "%s" RESET "\n", options[i]);
+            if (i == selected) printf("  " TUI_ACCENT "\xe2\x9d\xaf %s" TUI_RESET "\n", options[i]);
+            else printf("    " TUI_DIM "%s" TUI_RESET "\n", options[i]);
         }
         drawn = 1;
         fflush(stdout);
@@ -101,7 +101,7 @@ int tui_menu(const char **options, int count) {
 }
 
 void tui_wait_key(void) {
-    printf("\n  " DIM "press any key to continue" RESET);
+    printf("\n  " TUI_DIM "press any key to continue" TUI_RESET);
     fflush(stdout);
     _getch();
     printf("\n");
